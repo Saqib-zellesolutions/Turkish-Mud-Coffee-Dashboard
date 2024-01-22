6e313488dfa55f2177ddc6c3e1fef8b6f50053f5
abdul-sami-zelle <abdulsami.zellesolutions@gmail.com>
2024-01-22 11:55:40 +0500

    env end point change

--- src/config/env.js
+++ src/config/env.js
@@ -1,7 +1,7 @@
-export const LocalUrl = "http://localhost:4000/api/v1";
-export const ImageUrl = "http://localhost:4000";
-// export const LocalUrl = "https://testing2.zameeransari.com.pk/api/v1";
-// export const ImageUrl = "https://testing2.zameeransari.com.pk";
+// export const LocalUrl = "http://localhost:4000/api/v1";
+// export const ImageUrl = "http://localhost:4000";
+export const LocalUrl = "https://testing2.zameeransari.com.pk/api/v1";
+export const ImageUrl = "https://testing2.zameeransari.com.pk";
 export const BranchFunction = (branch) => {
   return branch === "branch1"
     ? 1
